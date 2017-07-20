c4706cdf770b762200cc3f17355e0b776b79b693
jb-matsunaga <jb.matsunaga@gmail.com>
2017-07-21 00:26:49 +0900

    Binding Action Creators

---
+++ src/containers/book-list.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
+
+class BookList extends Component {
+    renderList() {
+        return this.props.books.map((book) => {
+            return(
+                <li key={book.title} className="list-group-item">{book.title}</li>
+            )
+        })
+    }
+    render() {
+        return (
+            <ul className="list-group col-sm-4">
+                {this.renderList()}
+            </ul>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    // 返されたものは何でもBookListの中のpropsとして表示
+    return {
+        books: state.books
+    };
+}
+
+// この関数から返されたものは、BookListコンテナのpropsとして終了する
+function mapDispatchToProps(dispatch) {
+    // selectBookが呼び出されると、結果はすべてのreducerに渡される
+    return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+
+// BookListをコンポーネントからコンテナに昇格する
+// - この新しいdispatchメソッド,selectBookについて知る必要がある
+// それをpropとして利用可能にする
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
