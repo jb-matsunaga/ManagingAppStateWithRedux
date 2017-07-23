56b51f4a0334ba19d23278f199b9e8f50eaf4f8f
jb-matsunaga <jb.matsunaga@gmail.com>
2017-07-24 00:31:36 +0900

    reducerコメント追記

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,3 +6,8 @@ const rootReducer = combineReducers({
 });
 
 export default rootReducer;
+
+// すべてのreducerはそのactionを処理し、新しいstateを返す 
+// 新しいstateが組み立てられる 
+// コンテナにstateの変更を通知する 
+// 通知では、コンテナが新しいpropsで再レンダリングされる
